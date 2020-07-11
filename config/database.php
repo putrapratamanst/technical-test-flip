276d963fa6a1060d2ef6788c9741a7f1991c390e
Putra Pratama Nasution <putrapratamanst@gmail.com>
2020-07-11 20:43:19 +0700

    first commit

---
+++ config/database.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'disburse_db' => [
+        'db_connection' => 'mysql',
+        'db_host'       => '127.0.0.1',
+        'db_port'       => '33006',
+        'db_database'   => 'disburse_db',
+        'db_username'   => 'root',
+        'db_password'   => 'password',
+    ],
+];
