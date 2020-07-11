88773708cde2cd039bee243550e897cee08597f2
Putra Pratama Nasution <putrapratamanst@gmail.com>
2020-07-11 21:55:44 +0700

    config database

--- config/database.php
+++ config/database.php
@@ -4,9 +4,9 @@ return [
     'disburse_db' => [
         'db_connection' => 'mysql',
         'db_host'       => '127.0.0.1',
-        'db_port'       => '33006',
+        'db_port'       => '3306',
         'db_database'   => 'disburse_db',
-        'db_username'   => 'root',
-        'db_password'   => 'password',
+        'db_username'   => '',
+        'db_password'   => '',
     ],
 ];
